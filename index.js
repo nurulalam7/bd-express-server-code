2fc31bb5ee7f533119d626fe269facf8f1bced30
nurulalam7 <nurulalam7540@gmail.com>
2021-11-01 18:38:52 +0600

    add

--- index.js
+++ index.js
@@ -156,9 +156,7 @@ run().catch(console.dir);
 app.get('/',(req,res)=>{
     res.send('delivery is okay')
 });
-app.get('/hello',(req,res)=>{
-    res.send('delivery is okay now we are very happy')
-});
+
 
 app.listen(port,()=>{
     console.log('server running ',port)
